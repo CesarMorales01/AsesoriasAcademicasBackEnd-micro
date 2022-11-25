c40808b305c9c74397706cd18864227cb214fae9
@CesarMorales01 <cezar_mh86@hotmail.com>
2022-11-24 21:07:01 -0500

    Implementacion docker

--- controllers/Proyecto.js
+++ controllers/Proyecto.js
@@ -3,8 +3,9 @@ const proyectoModel = require('../models/Proyectos');
 
 const getProyecto = async (req, res = resp) => {
     try{
-        const proyectoBD = await proyectoModel.find();
-        res.json(proyectoBD);
+        console.log("Ambiente", process.env.CONTAINET_HOST)
+        const proyectoBD = await proyectoModel.find()
+        res.json(proyectoBD)
     }catch(e){
         return res.status(500).json({
             error: e
